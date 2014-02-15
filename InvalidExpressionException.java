b97a6848477ddc9d7ca0124f01bcedaac5c871b7
bobdole99 <mike.gugala@gmail.com>
2014-02-14 22:34:34 -0700

    Create InvalidExpressionException.java

---
+++ InvalidExpressionException.java
@@ -0,0 +1,9 @@
+/**
+InvalidExpressionException is used when an invalid expression is encountered
+*/
+
+public class InvalidExpressionException extends RuntimeException {
+    public InvalidExpressionException(String message){
+	    super(message);
+	}
+} 
